93d5f23d8fe9edbf39dcc1b72098b5646bdebef8
Anton Korobkin <3597052@gmail.com>
2023-04-30 14:27:47 +0300

    feat: add base file and webpack

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import "./scss/style.scss";
+
+const heading = document.createElement('h1');
+heading.textContent = 'Virtual Keyboard!';
+
+// добавляем заголовок в DOM
+const root = document.body;
+root.append(heading);
