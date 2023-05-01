2c309c0c3daa36ca92118f5f8bebf140c2af5dc7
Anton Korobkin <3597052@gmail.com>
2023-05-01 21:11:17 +0300

    feat: add keys in DOM and styled

--- src/components/app.js
+++ src/components/app.js
@@ -8,11 +8,31 @@ export default class App {
     textareaWrapper.classList.add('textarea-wrapper');
     const textarea = document.createElement('textarea');
     textareaWrapper.appendChild(textarea);
-    keyboardContainer.appendChild(textareaWrapper);
+    document.body.appendChild(textareaWrapper);
 
     Object.keys(keys).forEach(keyName => {
       const keyData = keys[keyName];
+      const keyElement = document.createElement('div');
+      keyElement.classList.add('key');
+      keyElement.dataset.key = keyName;
+      keyElement.dataset.layout = 'EN';
+
+      const position = keyData.position.split('*');
+      const row = position[0];
+      const coll = position[1];
+      const length = position[2];
+
+      keyElement.style.gridRow = row;
+      keyElement.style.gridColumn = `${coll} / span ${length}`;
+
+      if (keyData.value && keyData.value.EN) {
+        keyElement.textContent = keyData.value.EN;
+      } else {
+        console.error(`Missing EN value property for key ${keyName}`);
+      }
+
       console.log(keyName, keyData);
+      keyboardContainer.appendChild(keyElement);
     });
     document.body.appendChild(keyboardContainer);
   }
