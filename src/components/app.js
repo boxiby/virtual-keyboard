e73de691ce7e891c05d02e504f731811b0e12b8e
Anton Korobkin <3597052@gmail.com>
2023-05-01 15:47:32 +0300

    feat: output textarea in body

--- src/components/app.js
+++ src/components/app.js
@@ -2,9 +2,18 @@ import keys from './key';
 
 export default class App {
   output() {
+    const keyboardContainer = document.createElement('div');
+    keyboardContainer.classList.add('keyboard');
+    const textareaWrapper = document.createElement('div');
+    textareaWrapper.classList.add('textarea-wrapper');
+    const textarea = document.createElement('textarea');
+    textareaWrapper.appendChild(textarea);
+    keyboardContainer.appendChild(textareaWrapper);
+
     Object.keys(keys).forEach(keyName => {
       const keyData = keys[keyName];
       console.log(keyName, keyData);
     });
+    document.body.appendChild(keyboardContainer);
   }
 }
